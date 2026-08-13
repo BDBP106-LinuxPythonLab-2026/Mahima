3f667b5554403383e872ff2843a0e856d52e1b2b
Mahima17a <mahimashukla470@gmail.com>
2026-08-13 20:52:41 +0530

    lab9 submission

---
+++ lab9/name.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "My name is $1."
+echo "I am currently studying in $2 course."
+
+echo "The exit code is: $?"
+echo "The name of the script is: $0"
+echo "The number of arguments passed to this command is: $#"
+echo "The array of arguments passed to this script is: $@"
